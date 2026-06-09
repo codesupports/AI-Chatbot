216d5accd2833908fd45cdb953972d78db214dfc
codesupports <rajkumar.mah55@gmail.com>
2026-06-09 13:16:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section className='bg-gray-800'>
+        Test Template
+      </section>
+    </>
+  )
+}
+
+export default App
