8857d3ee56ea8950e9dc82ae24b8407b44304968
codesupports <rajkumar.mah55@gmail.com>
2026-06-09 13:45:36 +0530

    url change

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
   return (
     <>
       <section className='bg-blue-700'>
-        <h1 className='text-2xl'>Test Template</h1>
+        <h1 className='text-2xl'>Test Template Update nahi ho rha hai
+
+        </h1>
       </section>
     </>
   )
